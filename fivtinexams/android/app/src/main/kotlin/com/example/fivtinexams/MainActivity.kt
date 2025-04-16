14de7d05d6a2105e131e527eeedb68f80fb15f5c
Arga-12 <argarill226@gmail.com>
2025-04-16 14:17:17 +0700

    first commit

---
+++ fivtinexams/android/app/src/main/kotlin/com/example/fivtinexams/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fivtinexams
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ fivtinexams/lib/pages/home.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Beranda'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.home,
+              size: 100,
+              color: Color.fromARGB(255, 235, 144, 60),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Selamat datang di Fivtin Exams!',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.orange,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+
+            //Tambah tombol
+            ElevatedButton(
+              onPressed: () {
+                // Aksi saat tombol ditekan
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Tombol ditekan!'),
+                  ),
+                );
+              },
+              child: const Text('Tekan Aku'),
+            )
+          ],
+        ),)
+    );
+  }
+}
