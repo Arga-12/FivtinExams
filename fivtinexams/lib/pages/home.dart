568fe3b599aabc0ea96f70f61f27d0fedbb9ed69
Arga-12 <argarill226@gmail.com>
2025-04-16 19:21:34 +0700

    Pesan commit tentang perubahan yang dilakukan

--- fivtinexams/lib/pages/home.dart
+++ fivtinexams/lib/pages/home.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Beranda'),
+        title: const Text('Beranda Di Update!!!!'),
         backgroundColor: Colors.blue,
       ),
       body: Center(
